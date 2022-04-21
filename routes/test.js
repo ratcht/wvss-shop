be24d18dcf64fbef76b6c72c832e351d06221d52
Hynek Lukes <hyneklu@gmail.com>
2022-04-20 21:19:25 -0700

    work on order page and shop item

--- routes/test.js
+++ routes/test.js
@@ -8,5 +8,8 @@ router.get('/', function(req, res, next) {
 router.get('/product', function(req, res, next) {
   res.render('detail-product', {data: ['lol1','lol2','s','sss','ff', 'xsd'], title: 'Wvss Testing Crew'});
 });
+router.get('/orders', function(req, res, next) {
+  res.render('orders', {title: 'Wvss Testing Crew'});
+});
 
 module.exports = router;
