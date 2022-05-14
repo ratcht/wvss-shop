132825011f7516fdda18909b5c8c9940b8d3b8d9
Hynek Lukes <hyneklu@gmail.com>
2022-05-13 23:50:45 -0700

    layout-changes, added footer

--- routes/test.js
+++ routes/test.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('productpage-test', { data: ['lol1','lol2','s','sss','ff', 'xsd'], title: 'Wvss Testing Crew'});
 });
 router.get('/product', function(req, res, next) {
-  res.render('detail-product', {data: ['lol1','lol2','s','sss','ff', 'xsd'], title: 'Wvss Testing Crew'});
+  res.render('detail-product', {productData: ['lol1','lol2','s','sss','ff', 'xsd'], title: 'Wvss Testing Crew'});
 });
 router.get('/orders', function(req, res, next) {
   res.render('orders', {title: 'Wvss Testing Crew'});
